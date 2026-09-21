a2beb392fef7c2cd147f4c85fc834904187c665d
daneshi64 <daneshi64@gmail.com>
2026-09-21 11:50:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -188,7 +188,7 @@ def F(z, target):
 # --------------------------------------------------
 # Bisection method
 # --------------------------------------------------
-def find_z_bisection(target, tolerance=1e-8):
+def find_z_bisection(target, tolerance=1e-11):
 
     z_low = 0.0
     z_high = 5.0
